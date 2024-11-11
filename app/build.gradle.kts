70a45cb6a31340cec1e3a75fbb1ccfb1c88e6151
Yazid Bintang <yazidbintang2711@gmail.com>
2024-11-11 20:04:50 +0700

    Add Required Dependencies and connect to Firebase project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
    alias(libs.plugins.android.application)
    alias(libs.plugins.kotlin.android)
+   id("com.google.devtools.ksp")
+   id("com.google.dagger.hilt.android")
+   id("androidx.navigation.safeargs.kotlin")
+   alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -34,6 +38,10 @@ android {
    kotlinOptions {
       jvmTarget = "1.8"
    }
+   buildFeatures {
+      viewBinding = true
+      buildConfig = true
+   }
 }
 
 dependencies {
@@ -46,4 +54,45 @@ dependencies {
    testImplementation(libs.junit)
    androidTestImplementation(libs.androidx.junit)
    androidTestImplementation(libs.androidx.espresso.core)
+
+   //Dagger Hilt
+   implementation(libs.hilt.android)
+   ksp(libs.dagger.compiler) // Dagger compiler
+   ksp(libs.hilt.compiler)   // Hilt compiler
+
+   // Retrofit
+   implementation(libs.retrofit)
+   implementation(libs.converter.gson)
+
+   // OkHttp and Chucker
+   implementation(libs.okhttp)
+   debugImplementation(libs.library)
+   releaseImplementation(libs.library.no.op)
+
+   // Lifecycle
+   implementation(libs.androidx.lifecycle.viewmodel.ktx)
+   implementation(libs.androidx.lifecycle.livedata.ktx)
+   implementation(libs.androidx.lifecycle.runtime.ktx)
+
+   // Coroutines
+   implementation (libs.kotlinx.coroutines.android)
+
+   // Glide
+   implementation(libs.glide)
+   annotationProcessor(libs.compiler)
+
+   // CameraX
+   implementation(libs.androidx.camera.camera2)
+   implementation(libs.androidx.camera.lifecycle)
+   implementation(libs.androidx.camera.view)
+
+   // Jetpack Navigation
+   implementation(libs.androidx.navigation.fragment)
+   implementation(libs.androidx.navigation.ui)
+
+   // DataStore
+   implementation(libs.androidx.datastore.preferences)
+
+   // Firebase Auth
+   implementation(libs.firebase.auth)
 }
