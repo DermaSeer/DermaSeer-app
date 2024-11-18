3e674d84564496396a6552e890c920dca4b16c0b
Yazid Bintang <yazidbintang2711@gmail.com>
2024-11-19 00:16:39 +0700

    Create UI for splash screen, signin, home, complete profile and change app icon

--- app/src/main/java/com/dermaseer/dermaseer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dermaseer/dermaseer/ui/home/HomeFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.dermaseer.dermaseer.R
+import com.dermaseer.dermaseer.databinding.FragmentHomeBinding
+import com.dermaseer.dermaseer.databinding.ItemProductTypeBinding
 
 class HomeFragment : Fragment() {
+
+   private var _binding: FragmentHomeBinding? = null
+   private val binding get() = _binding!!
+   private lateinit var navController: NavController
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
@@ -16,12 +24,45 @@ class HomeFragment : Fragment() {
    override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?,
       savedInstanceState: Bundle?
-   ): View? {
+   ): View {
       // Inflate the layout for this fragment
-      return inflater.inflate(R.layout.fragment_home, container, false)
+      _binding = FragmentHomeBinding.inflate(inflater, container, false)
+      return binding.root
    }
 
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
+      navController = Navigation.findNavController(view)
+      setProductType()
+   }
+
+   private fun setProductType() {
+      val listProductType = listOf(
+         ProductType(R.drawable.moisturizer, requireContext().getString(R.string.moisturizer)),
+         ProductType(R.drawable.toner, requireContext().getString(R.string.toner)),
+         ProductType(R.drawable.serum, requireContext().getString(R.string.serum)),
+         ProductType(R.drawable.facewash, requireContext().getString(R.string.facewash)),
+         ProductType(R.drawable.sunscreen, requireContext().getString(R.string.sunscreen)),
+      )
+
+      val listCard: List<ItemProductTypeBinding>
+      with (binding) {
+         listCard = listOf(cardMoisturizer, cardToner, cardSerum, cardFacewash, cardSunscreen)
+      }
+
+      for (i in listProductType.indices) {
+         listCard[i].tvProductType.text = listProductType[i].productTitle
+         listCard[i].tvProductImage.setImageResource(listProductType[i].productImage)
+      }
+   }
+
+   override fun onDestroyView() {
+      super.onDestroyView()
+      _binding = null
    }
 }
+
+data class ProductType (
+   val productImage: Int,
+   val productTitle: String
+)
