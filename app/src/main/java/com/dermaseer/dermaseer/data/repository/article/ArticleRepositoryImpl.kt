ad2aa5adb55e9b62dc7c197e9da80f6041485dad
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-11-24 17:45:00 +0700

    Update ArticleRepositoryImpl.kt

--- app/src/main/java/com/dermaseer/dermaseer/data/repository/article/ArticleRepositoryImpl.kt
+++ app/src/main/java/com/dermaseer/dermaseer/data/repository/article/ArticleRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.dermaseer.dermaseer.data.repository.article
 
+import android.util.Log
 import com.dermaseer.dermaseer.data.local.datastore.AuthPreferences
 import com.dermaseer.dermaseer.data.remote.models.ArticleResponse
 import com.dermaseer.dermaseer.data.remote.services.ArticleService
@@ -7,11 +8,18 @@ import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class ArticleRepositoryImpl @Inject constructor(
+   private val apiService: ArticleService,
    private val authPreferences: AuthPreferences
 ) : ArticleRepository {
+
    override suspend fun getAllArticle(): ArticleResponse {
       val token = authPreferences.authToken.first()
+
+      return if (token.isNullOrEmpty()) {
+         throw IllegalStateException("Token is null or empty")
       } else {
+         apiService.getAllArticle("Bearer $token")
       }
    }
+
 }
